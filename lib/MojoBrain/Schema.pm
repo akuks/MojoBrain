a880ec735ff7a8a5e40a214587777ca6a351a7fe
Ashutosh <kukreti.ashutosh@gmail.com>
2021-06-30 15:16:41 +0530

    Add Schema

---
+++ lib/MojoBrain/Schema.pm
@@ -0,0 +1,20 @@
+use utf8;
+package MojoBrain::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-06-29 20:00:54
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7v8hVMF912RNnKtTy2rgTw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
