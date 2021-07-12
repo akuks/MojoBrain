60312860c6710566417cf6dbf571f0baf6408564
Ashutosh <kukreti.ashutosh@gmail.com>
2021-07-12 19:25:51 +0530

    Initial Authentication

--- lib/MojoBrain/Controller/Admin/Auth.pm
+++ lib/MojoBrain/Controller/Admin/Auth.pm
@@ -6,5 +6,32 @@ sub signin($c) {
   $c->render(template => 'layouts/admin/signin')
 }
 
+sub signin_post ($c) {
+  my $username = $c->param('username');
+  my $password = $c->param('password');
+
+  unless ($c->is_user_exists($username, $password)) {
+    $c->flash( error => $c->app->msg->get_invalid_credentials_message );
+    return $c->redirect_to('/admin/login');
+  }
+
+}
+
+sub register($c) {
+  $c->render(template => 'layouts/admin/register')
+}
+
+sub is_user_exists($c, $username, $password) {
+  my $user = $c->app->db->resultset('User')->search({
+    email => $username
+  });
+  return 0 if !$user->count;
+
+  $user = $user->first;
+  
+  return $c->bcrypt_validate($password || '', $user->password);
+}
+
+
 
 1;
