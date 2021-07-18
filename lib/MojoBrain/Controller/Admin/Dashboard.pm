791278cef1ebba1394fd6bb6ba67a46a8fcd02a5
Ashutosh <kukreti.ashutosh@gmail.com>
2021-07-18 11:10:10 +0530

    Pass user object to admin dashboard

--- lib/MojoBrain/Controller/Admin/Dashboard.pm
+++ lib/MojoBrain/Controller/Admin/Dashboard.pm
@@ -2,8 +2,16 @@ package MojoBrain::Controller::Admin::Dashboard;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub dashboard($c) {
-  print $c->session('user_exists'), "\n";
-  $c->render(template => 'admin/dashboard');
+  my $user = $c->get_user_details;
+  
+  $c->render(template => 'admin/dashboard', user => $user);
+}
+
+# Subroutines
+sub get_user_details($c) {
+  return $c->session('user_id') ? $c->db->resultset('User')->search({
+    user_id => $c->session('user_id')
+  })->first : '';
 }
 
 1;
