8b597bd640fab385f9cbd7dc7351ae6598d812e7
Ashutosh <kukreti.ashutosh@gmail.com>
2021-08-19 00:08:52 +0530

    Left Panel Javascript for Admin Dashboard

--- public/js/admin/left_panel.js
+++ public/js/admin/left_panel.js
@@ -2,3 +2,10 @@
 document.getElementById('mb-chevron-icon').onclick = function(e){
   alert('click');
 }
+
+// Add Class float-right to mb-plus-icon
+let plusIconElement = document.getElementById('mb-plus-icon');
+plusIconElement.classList.add('float-right');
+plusIconElement.classList.add('text-gray-400');
+
+console.log(mojoBrain.addClassToElement());
