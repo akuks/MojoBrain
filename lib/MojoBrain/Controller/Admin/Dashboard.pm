ed30709f4b5e634d127f10e1eff980ca4ff75840
Ashutosh <kukreti.ashutosh@gmail.com>
2021-08-29 00:52:26 +0530

    Refactor Dashboard

--- lib/MojoBrain/Controller/Admin/Dashboard.pm
+++ lib/MojoBrain/Controller/Admin/Dashboard.pm
@@ -2,16 +2,9 @@ package MojoBrain::Controller::Admin::Dashboard;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub dashboard($c) {
-  my $user = $c->get_user_details;
+  my $user = $c->app->get_user_details;
   
   $c->render(template => 'admin/dashboard', user => $user);
-}
-
-# Subroutines
-sub get_user_details($c) {
-  return $c->session('user_id') ? $c->db->resultset('User')->search({
-    user_id => $c->session('user_id')
-  })->first : '';
 }
 
 1;
