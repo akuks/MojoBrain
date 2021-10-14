9180cf712348d6980024db93f43e7a91db99e808
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-14 15:23:05 +0530

    Update dashboard to send user_id

--- lib/MojoBrain/Controller/Admin/Dashboard.pm
+++ lib/MojoBrain/Controller/Admin/Dashboard.pm
@@ -2,7 +2,7 @@ package MojoBrain::Controller::Admin::Dashboard;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub dashboard($c) {
-  my $user = $c->app->get_user_details;
+  my $user = $c->app->get_user_details(  $c->session('user_id') );
   
   $c->render(template => 'admin/dashboard', user => $user);
 }
