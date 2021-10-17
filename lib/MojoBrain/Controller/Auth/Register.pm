3dfc585db9ebf71a51d551beeb9b4ea536c56eca
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-17 05:41:04 +0530

    Add register parameters validations

--- lib/MojoBrain/Controller/Auth/Register.pm
+++ lib/MojoBrain/Controller/Auth/Register.pm
@@ -1,6 +1,7 @@
 package MojoBrain::Controller::Auth::Register;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use JSON;
+use Email::Valid;
 
 # This action will render a template
 # Not used. Need to move in a separate folder
@@ -21,6 +22,40 @@ sub register ($c) {
 
 # This action will render a template
 sub signup ($c) {
+  $c->render( template => 'auth/signup' )
+}
+
+
+sub signup_post ($c) {
+
+  my $v = $c->validation;
+  $v = $v->csrf_protect;
+
+  return $c->render(text => 'Bad CSRF token!', status => 403) if $v->csrf_protect->has_error('csrf_token');
+
+  $v->required('name')->like(q/[a-zA-Z]/);
+  $v->required('email')->like( Email::Valid->address( $c->param('email') ) );
+  $v->required('password')->like(qr/^[a-z0-9]+$/);
+  $v->required('confirm_password')->equal_to('password') if $v->optional('password')->size(7, 500)->is_valid;
+
+  my %options;
+  
+  # Validation has any errors
+  if ( $v->has_error ) {
+    $c->flash( name_error => $v->error('name') );
+    $c->flash( email_error => $v->error('email') );
+    $c->flash( pass_error => $v->error('password') );
+    $c->flash( conf_pass_error => $v->error('confirm_password') );
+    return $c->redirect_to( '/signup' );
+  }
+  
+  %options = (
+    name  => $c->param('name'),
+    email => $c->param('email')
+  );
+
+  return $c->redirect_to( '/signup' );
+
   $c->render( template => 'auth/signup' )
 }
 
