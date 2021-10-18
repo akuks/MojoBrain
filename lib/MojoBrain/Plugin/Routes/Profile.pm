430fa24745375222a59acefd1cafbcc6dcf59036
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-18 14:06:55 +0530

    Profile Routes Plugin

---
+++ lib/MojoBrain/Plugin/Routes/Profile.pm
@@ -0,0 +1,14 @@
+package MojoBrain::Plugin::Routes::Profile;
+use Mojo::Base 'Mojolicious::Plugin', -signatures;
+
+sub register ($self, $app, $config) {
+  my $r = $app->routes; 
+  
+  my $auth = $r->under('/profile')->to('Auth::Login#is_user_authenticated');
+  $auth->get('/')->to('Auth::Profile#profile');
+  #$auth->post('/')->to('Auth::Profile#profile_post');
+
+  return;
+}
+
+1;
