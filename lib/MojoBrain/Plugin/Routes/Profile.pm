eccf035e200f6b4e18d45db5e7c4b5f35b960fb4
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-20 00:19:46 +0530

    uncommented post profile url

--- lib/MojoBrain/Plugin/Routes/Profile.pm
+++ lib/MojoBrain/Plugin/Routes/Profile.pm
@@ -6,7 +6,7 @@ sub register ($self, $app, $config) {
   
   my $auth = $r->under('/profile')->to('Auth::Login#is_user_authenticated');
   $auth->get('/')->to('Auth::Profile#profile');
-  #$auth->post('/')->to('Auth::Profile#profile_post');
+  $auth->post('/')->to('Auth::Profile#profile_post');
 
   return;
 }
