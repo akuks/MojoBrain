514b8e904b690804b7339a6d88af503640724448
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-22 00:44:33 +0530

    add change_password route

--- lib/MojoBrain/Plugin/Routes/Profile.pm
+++ lib/MojoBrain/Plugin/Routes/Profile.pm
@@ -7,6 +7,7 @@ sub register ($self, $app, $config) {
   my $auth = $r->under('/profile')->to('Auth::Login#is_user_authenticated');
   $auth->get('/')->to('Auth::Profile#profile');
   $auth->post('/')->to('Auth::Profile#profile_post');
+  $auth->post('/change_password')->to('Auth::Profile#change_password_post');
 
   return;
 }
