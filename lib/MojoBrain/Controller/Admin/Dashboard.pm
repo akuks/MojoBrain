ad2fee524ccf6d78cdc8478b05d855b032c28139
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-22 17:35:08 +0530

    Add module to highlight the changes

--- lib/MojoBrain/Controller/Admin/Dashboard.pm
+++ lib/MojoBrain/Controller/Admin/Dashboard.pm
@@ -5,6 +5,7 @@ sub dashboard($c) {
   my $user = $c->app->get_user_details(  $c->session('user_id') );
   
   $c->stash( user => $user );
+  $c->stash( module => 'Dashboard' );
   $c->render( template => 'admin/dashboard' );
 }
 
