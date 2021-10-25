a876ccae6484e99efc0f972d2c3af4ed6d87494a
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-25 14:27:42 +0530

    Fix function call

--- lib/MojoBrain/Controller/Admin/Dashboard.pm
+++ lib/MojoBrain/Controller/Admin/Dashboard.pm
@@ -2,7 +2,7 @@ package MojoBrain::Controller::Admin::Dashboard;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub dashboard($c) {
-  my $user = $c->app->get_user_details(  $c->session('user_id') );
+  my $user = $c->app->db->resultset('User')->get_user_details(  $c->session('user_id') );
   
   $c->stash( user => $user );
   $c->stash( module => 'Dashboard' );
