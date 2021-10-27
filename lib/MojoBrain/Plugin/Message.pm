75bec6bb1ec95a31b28004b2dabe0ef807b438a3
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-27 21:20:37 +0530

    Helper function init.

--- lib/MojoBrain/Plugin/Message.pm
+++ lib/MojoBrain/Plugin/Message.pm
@@ -2,8 +2,10 @@ package MojoBrain::Plugin::Message;
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 
 sub register ($self, $app, $config) {
-
-  
+  my %message;
+  $app->helper( message => sub ( $v, $key ) {
+    return $message { $key };
+  });
 
   return;
 }
