d63a004c7b80da2b209dae16d92923b2584e344c
Ashutosh <kukreti.ashutosh@gmail.com>
2021-10-30 04:28:27 +0530

    Update list of client function

--- lib/MojoBrain/Controller/Admin/Client.pm
+++ lib/MojoBrain/Controller/Admin/Client.pm
@@ -2,7 +2,11 @@ package MojoBrain::Controller::Admin::Client;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub client ($c) {
+  my $details = $c->app->db->resultset('Client')->get_client_details( $c->session('user_id') );
+
   $c->stash( 'module' => 'Clients' );
+  $c->stash( 'details' => $details );
+  
   $c->render( template => '/admin/client')
 }
 
