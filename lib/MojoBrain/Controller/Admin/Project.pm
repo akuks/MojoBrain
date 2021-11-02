79f7aac05a4514db3f76b56a0799155ffa48b1d3
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-02 23:49:54 +0530

    Set active class when url is projects

--- lib/MojoBrain/Controller/Admin/Project.pm
+++ lib/MojoBrain/Controller/Admin/Project.pm
@@ -4,7 +4,7 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 sub project ($c) {
   # my $details = $c->app->db->resultset('Client')->get_client_details( $c->session('user_id') );
 
-  # $c->stash( 'module' => 'Clients' );
+  $c->stash( 'module' => 'Projects' );
   # $c->stash( 'details' => $details );
   
   $c->render( template => '/admin/project')
