4ede201b9b249af154918f9d3336ae03d6015ad4
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-05 18:43:32 +0530

    Segregate create and update project logic

--- lib/MojoBrain/Schema/ResultSet/Project.pm
+++ lib/MojoBrain/Schema/ResultSet/Project.pm
@@ -6,16 +6,27 @@ use base 'DBIx::Class::ResultSet';
 use Mojo::Base 'Mojolicious', -signatures;
 
 sub create_update_project ( $self, $options ) {
-  my $client;
+  my $project;
 
-  eval {
-    $client = $self->update_or_create(
-      { %$options },
-      { key => 'project_id' }
-    );
-  };
-  
-  return $@ ? 0 : $client;
+  if ( $options->{ project_id } ) {
+    # When project needs to be updated
+    eval {
+      $project = $self->update_or_create(
+        { %$options },
+        { key => 'project_id' }
+      );
+    };
+  }
+  else {
+    # For newly created projects
+    eval {
+      $project = $self->create(
+        { %$options }
+      );
+    };
+  }
+
+  return $@ ? 0 : $project;
 }
 
 sub get_project_details_by_user ( $self, $user ) {
