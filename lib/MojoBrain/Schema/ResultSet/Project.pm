458ea33c4ad2eaa6ac4dd8fa5e381c7f7d894610
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-05 18:04:08 +0530

    create and update project

---
+++ lib/MojoBrain/Schema/ResultSet/Project.pm
@@ -0,0 +1,38 @@
+package MojoBrain::Schema::ResultSet::Project;
+use strict;
+use warnings;
+use base 'DBIx::Class::ResultSet';
+
+use Mojo::Base 'Mojolicious', -signatures;
+
+sub create_update_project ( $self, $options ) {
+  my $client;
+
+  eval {
+    $client = $self->update_or_create(
+      { %$options },
+      { key => 'project_id' }
+    );
+  };
+  
+  return $@ ? 0 : $client;
+}
+
+sub get_project_details_by_user ( $self, $user ) {
+  my @projects = $self->search(
+    { user_id => $user }
+  );
+
+  my @details = map { {
+      name         => $_->name,
+      email        => $_->email,
+      status       => $_->status,
+      telephone    => $_->telephone,
+      company_name => $_->company_name,
+      client_id    => $_->client_id
+  } } @projects;
+
+  return \@details
+}
+
+1;
