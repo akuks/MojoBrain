44a6661540ec62626bed6bfe119ada5e00c5ffa3
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-08 22:40:52 +0530

    Extend Task Class Schema

---
+++ lib/MojoBrain/Schema/ResultSet/Task.pm
@@ -0,0 +1,43 @@
+package MojoBrain::Schema::ResultSet::Task;
+use strict;
+use warnings;
+use base 'DBIx::Class::ResultSet';
+
+use Mojo::Base 'Mojolicious', -signatures;
+
+sub create_update_task ( $self, $options ) {
+  my $task;
+
+  if ( $options->{ task_id } ) {
+    # When project needs to be updated
+    eval {
+      $task = $self->update_or_create(
+        { %$options },
+        { key => 'project_id' }
+      );
+    };
+  }
+  else {
+    # For newly created task
+    eval {
+      $task = $self->create(
+        { %$options }
+      );
+    };
+  }
+  print Data::Dumper::Dumper($@);
+  return $@ ? 0 : $task;
+}
+
+sub get_task_by_project_id ( $self, $project_id, $title, $user_id ) {
+  my $task = $self->find({
+    project_id => $project_id,
+    title      => $title,
+    created_by => $user_id
+  });
+
+  # If not defined it'll return undef
+  return $task; 
+}
+
+1;
