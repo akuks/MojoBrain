8eb1c8b43f10bd25d703bbd307d5796417138490
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-15 18:40:20 +0530

    Add unique constraint

--- lib/MojoBrain/Schema/Result/Task.pm
+++ lib/MojoBrain/Schema/Result/Task.pm
@@ -190,4 +190,7 @@ __PACKAGE__->belongs_to(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+__PACKAGE__->add_unique_constraint("task_id", ["task_id"]);
+
 1;
