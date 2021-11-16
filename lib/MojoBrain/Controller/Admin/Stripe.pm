4bbdb690fa1b8922479ef2974a87f0bdbdb65f44
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-16 19:11:36 +0530

    Stripe controller

---
+++ lib/MojoBrain/Controller/Admin/Stripe.pm
@@ -0,0 +1,40 @@
+package MojoBrain::Controller::Admin::Stripe;
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+
+sub stripe ( $c ) {
+  $c->stash( 'module' => 'Stripe' );
+
+  $c->render( template => 'admin/stripe' );
+}
+
+sub update_stripe ( $c ) {
+
+  my $publish_key = $c->param( 'publish_key' );
+  my $secret_key  = $c->param( 'private_key' );
+
+  my %options = (
+    publish_key => $c->param( 'publish_key' ),
+    secret_key  => $c->param( 'secret_key' )
+  );
+
+  return $c->render( error => 'Private and Publish keys cannot be left blank or null' ) 
+    if ( !$options { publish_key } or !$options { secret_key });
+
+  my $output;
+
+  eval {
+    $c->app->db->resultset('Stripe')->update_or_create({ %options });
+  };
+
+  # If Stripe keys not updated
+  return $c->render ( error => 'Failed to update stripe keys' ) if $@;
+
+  $output->{ message } = 'Stripe Keys updated succesfully.';
+  $output->{ status } = 200;
+
+  $c->render( json => $output );
+
+}
+
+
+1;
