0dcbe66535c9dab7080ed4c3b05a48e97de0418a
Ashutosh <kukreti.ashutosh@gmail.com>
2021-11-16 19:11:13 +0530

    Add Stripe Route to update

--- lib/MojoBrain/Plugin/Routes/AdministratorDashboard.pm
+++ lib/MojoBrain/Plugin/Routes/AdministratorDashboard.pm
@@ -4,8 +4,13 @@ use Mojo::Base 'Mojolicious::Plugin', -signatures;
 sub register ($self, $app, $config) {
   my $r = $app->routes; 
   
-  my $admin = $r->under('/admin/dashboard/');
+  # Check if user is authenticated
+  my $admin = $r->under('/admin/dashboard/')->to('Auth::Login#is_user_authenticated');
   $admin->get('/post')->to('Admin::Post#get_post');
+
+  # Stripe Related Data in Admin Dashboard
+  $admin->get('/stripe')->to('Admin::Stripe#stripe');
+  $admin->post('/stripe')->to('Admin::Stripe#update_stripe');
 
   return;
 }
